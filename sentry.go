e790a3434f831e93039c5aa8ec808029465ccd5c
Soheil Koushan <soheil.koushan@gmail.com>
2015-12-04 13:12:42 -0800

    Refactor capture into its own method

--- sentry.go
+++ sentry.go
@@ -12,21 +12,22 @@ func Wrapper(job string, w func(string, ...interface{}) error) func(string, ...i
 	return func(queue string, args ...interface{}) error {
 		defer func() {
 			if e := recover(); e != nil { // capture panics
-				eStr := fmt.Sprint(e)
-				packet := raven.NewPacket(eStr, raven.NewException(errors.New(eStr), raven.NewStacktrace(1, 3, nil)))
-				raven.Capture(packet, map[string]string{"job": job, "logger": "resque"})
+				capture(job, fmt.Sprint(e), args...)
 			}
 		}()
 
 		err := w(queue, args...)
 
 		if err != nil { // capture errors returned by job
-			packet := raven.NewPacket(err.Error(), raven.NewException(err, raven.NewStacktrace(1, 3, nil)))
-			packet.Extra["Job"] = job
-			packet.Extra["Arguments"] = args
-			raven.Capture(packet, map[string]string{"job": job, "logger": "resque"})
+			capture(job, err.Error(), args...)
 		}
-
 		return err
 	}
+}
+
+func capture(job string, msg string, args ...interface{}) {
+	packet := raven.NewPacket(msg, raven.NewException(errors.New(msg), raven.NewStacktrace(2, 3, raven.IncludePaths())))
+	packet.Extra["Job"] = job
+	packet.Extra["Arguments"] = args
+	raven.Capture(packet, map[string]string{"job": job, "logger": "resque"})
 }
